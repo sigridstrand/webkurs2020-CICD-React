2a3d9aeff489c0e319d74db92f9de607c3d80f42
sigridstrand <sigridhstrand@gmail.com>
2020-10-21 17:43:46 +0200

    Updates

---
+++ precache-manifest.db55e3bb271d9f6fc0fae3881123ec86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc48eb5a1e16c2ce7188965fdecf425b",
+    "url": "/webkurs2020-CICD-React/index.html"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/webkurs2020-CICD-React/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "681ade4e15dcaf7df23c",
+    "url": "/webkurs2020-CICD-React/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/webkurs2020-CICD-React/static/js/2.16ab8709.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webkurs2020-CICD-React/static/js/2.16ab8709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "681ade4e15dcaf7df23c",
+    "url": "/webkurs2020-CICD-React/static/js/main.101481df.chunk.js"
+  },
+  {
+    "revision": "580e99ae8bc13b712167",
+    "url": "/webkurs2020-CICD-React/static/js/runtime-main.89e14c0b.js"
+  }
+]);
